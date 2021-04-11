d89e584b218b73e7531f84986af21e3328404b2a
Administrator <1126452933@qq.com>
2021-04-11 19:07:46 +0800

    first commit

---
+++ src/test/java/N242_ValidAnagramTest.java
@@ -0,0 +1,25 @@
+package ; 
+
+import org.junit.Test; 
+
+/** 
+* N242_ValidAnagram Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>4ÔÂ 11, 2021</pre> 
+* @version 1.0 
+*/ 
+public class N242_ValidAnagramTest { 
+
+/** 
+* 
+* Method: isAnagram(String s, String t) 
+* 
+*/ 
+@Test
+public void testIsAnagram() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
